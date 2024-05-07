808c62f3d07e35d5656dc63432d1ea5b55676c4d
omrzcn <omrzcnn@gmail.com>
2024-05-07 18:26:59 +0300

    checking queries

---
+++ src/main/java/com/cydeo/QueryDemo.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class QueryDemo implements CommandLineRunner {
+
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
