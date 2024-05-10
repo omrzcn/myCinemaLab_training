909bfc7c2a0ea7fd98aa20bccd1698f9c1ba40ed
omrzcn <omrzcnn@gmail.com>
2024-05-10 16:10:09 +0300

    queries

--- src/main/java/com/cydeo/repository/GenreRepository.java
+++ src/main/java/com/cydeo/repository/GenreRepository.java
@@ -2,7 +2,10 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
@@ -11,10 +14,14 @@ public interface GenreRepository extends JpaRepository<Genre, Long> {
 
     //Write a JPQL query that return all genres
 
+    @Query("select g from Genre g ")
+    List<Genre> retrieveAllGenre();
+
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns genres by containing name
-
+    @Query("select g.name from Genre g" )
+    List<String> retrieveGenreByName();
 
 }
